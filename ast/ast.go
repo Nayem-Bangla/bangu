f73eca2ced028e2e80d511cfb74d80402ede5172
Nayem-Bangla <nayemarafat786@gmail.com>
2025-08-18 13:03:56 +0000

    Add AST and perser

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "bangu/token"
+import (
+	"bangu/token"
+	"bytes"
+)
 
 type Node interface { // TokenLiteral returns the literal value of the token associated with the node.
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -67,3 +71,66 @@ func (i *Identifier) TokenLiteral() string {
 
 // expressionNode is a marker method to distinguish Identifier as an expression.
 func (i *Identifier) expressionNode() {}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+// TokenLiteral returns the literal value of the token associated with the ExpressionStatement node.
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
+
+// statementNode is a marker method to distinguish ExpressionStatement as a statement.
+func (es *ExpressionStatement) statementNode() {}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+// Stirng returns a string representation of the Program node.
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+func (i *Identifier) String() string {
+	return i.Value
+}
