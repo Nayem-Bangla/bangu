7482bdb27a319fc1f562aba32bcbaf34a711ef46
Nayem-Bangla <nayemarafat786@gmail.com>
2025-08-21 07:35:59 +0000

    Adding Boolean Literals, Null, Prefix Expressions, Infix Expressions, conditions to evaluator file

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"bangu/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch n := node.(type) {
 	// Statements
@@ -15,6 +21,19 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: n.Value}
+	case *ast.Boolean:
+		return nativeBoolToBoolleanObject(n.Value)
+	case *ast.PrefixExpression:
+		right := Eval(n.Right)
+		return evalPrefixExpression(n.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(n.Left)
+		right := Eval(n.Right)
+		return evalInfixExpression(n.Operator, left, right)
+	case *ast.BlockStatement:
+		return evalStatements(n.Statements)
+	case *ast.IfExpression:
+		return evalIfExpression(n)
 
 	}
 
@@ -29,4 +48,114 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+func nativeBoolToBoolleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
+
+func evalInfixExpression(
+	operator string, left, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBoolToBoolleanObject(left == right)
+	case operator == "!=":
+		return nativeBoolToBoolleanObject(left != right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(
+	operator string, left, right object.Object) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	switch operator {
+	case "+":
+		return &object.Integer{Value: leftVal + rightVal}
+	case "-":
+		return &object.Integer{Value: leftVal - rightVal}
+	case "*":
+		return &object.Integer{Value: leftVal * rightVal}
+	case "/":
+		if rightVal == 0 {
+			return NULL // Handle division by zero
+		}
+		return &object.Integer{Value: leftVal / rightVal}
+
+	case "<":
+		return nativeBoolToBoolleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBoolToBoolleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBoolleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBoolleanObject(leftVal != rightVal)
+	default:
+		return NULL // Unsupported operator
+	}
+
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true // Non-nil, non-boolean objects are considered truthy
+	}
 }
